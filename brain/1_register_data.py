6218c2bb486ad062844acf2a23c87d17d0c69888
charleygros <charley.gros@gmail.com>
2018-10-08 11:47:19 +1100

    add loop for registration

--- brain/1_register_data.py
+++ brain/1_register_data.py
@@ -14,15 +14,24 @@ import pandas as pd
 
 from config_file import config
 
-DATE_TIME_STG = datetime.datetime.now().strftime("%Y%m%d%H%M")
 
 def main(args=None):
 
     subj_data_df = pd.read_pickle('0_results.pkl')
 
     path_data = config['path_data']
+    center_dct = config["dct_center"]
+    current_dir = os.getcwd()
 
     # loop and run 1_register_data.sh
+    for index, row in subj_data_df.iterrows():
+    	anat_name = center_dct[row.center]['anat']
+    	subj_fold = os.path.join(path_data, row.subject, 'brain')
+    	flair_mni = os.path.join(subj_fold, anat_name, anat_name + '_mni.nii.gz')
+    	if not os.path.isfile(flair_mni):
+    		print flair_mni
+    		os.system('./1_register_data_anima.sh ' + subj_fold)
+    		os.chdir(current_dir)
 
 
 if __name__ == "__main__":
