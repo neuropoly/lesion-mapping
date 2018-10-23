083c482765373cf2f1b2e931788ce8d8b9be6ab3
charleygros <charley.gros@gmail.com>
2018-10-23 19:34:32 +1100

    output brain/1_results.pkl

--- brain/1_register_data.py
+++ brain/1_register_data.py
@@ -29,17 +29,22 @@ def main(args=None):
     current_dir = os.getcwd()
 
     # loop and run 1_register_data.sh
+    reg_subj_lst = []
     for index, row in subj_data_df.iterrows():
-        print row.subject
         anat_name = center_dct[row.center]['anat']
         subj_fold = os.path.join(path_data, row.subject, 'brain')
         flair_mni = os.path.join(subj_fold, anat_name, anat_name + '_mni.nii.gz')
         label_folder = os.path.join(subj_fold, anat_name, 'label')
+        brain_atlas_reg_path = os.path.join(subj_fold, anat_name, 'label', 'brain.nii.gz')
         if not os.path.isdir(label_folder):
             print flair_mni
             os.system('./1_register_data.sh ' + subj_fold + ' ' + path_atlases + ' ' + path_script_brain_extraction)
             os.chdir(current_dir)
+        if os.path.isfile(brain_atlas_reg_path):
+            reg_subj_lst.append(row.subject)
 
+    subj_data_df = subj_data_df[subj_data_df.subject.isin(reg_subj_lst)]
+    subj_data_df.to_pickle('1_results.pkl')
 
 if __name__ == "__main__":
     main()
