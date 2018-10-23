a1126ae8752e9b82312e0eb6b72c87c24a59bc6f
charleygros <charley.gros@gmail.com>
2018-10-23 15:10:46 +1100

    generate binary brain and brainstem atlas masks

--- brain/0_check_data.py
+++ brain/0_check_data.py
@@ -14,10 +14,13 @@
 #                                 anat.nii.gz # raw image
 #                                 anat_lesion_manual.nii.gz # lesion mask (binary)
 # (3) Creation of pickle '0_results.pkl', saved in current working directory. 
-# WARNING: If a file is missing, the subject will be excluded for the remainder of the pipeline. 
+# WARNING: If a file is missing, the subject will be excluded for the remainder of the pipeline.
+#
+# (4) Custom brain and brainstem atlases for the purpose of this study.
+# Save them in config["path_atlases"].
 #
 # Created: 2017-04-01
-# Modified: 2018-10-01
+# Modified: 2018-10-20
 # Contributors: Charley Gros, Sara Dupont & Dominique Eden
 
 import os
@@ -25,12 +28,90 @@ import pandas as pd
 import datetime
 import pickle
 import numpy as np
-from spinalcordtoolbox.image import Image
+from spinalcordtoolbox.image import Image, zeros_like
 
 from config_file import config
 
 DATE_TIME_STG = datetime.datetime.now().strftime("%Y%m%d%H%M")
 
+BRAINSTEM_DCT = {'CST_R': 'CSTR_Atlas.nii.gz',
+                'CST_L': 'CSTL_Atlas.nii.gz',
+                'CST': ''}
+BRAMSTEM_ZTOP = 51
+BRAMSTEM_ZBOT = 14
+
+BRAIN_DCT = {'M1_R': 'Right-M1-S-MATT.nii',
+            'M1_L': 'Left-M1-S-MATT.nii',
+            'PMd_R': 'Right-PMd-S-MATT.nii',
+            'PMd_L': 'Left-PMd-S-MATT.nii',
+            'PMv_R': 'Right-PMv-S-MATT.nii',
+            'PMv_L': 'Left-PMv-S-MATT.nii',
+            'preSMA_R': 'Right-preSMA-S-MATT.nii',
+            'preSMA_L': 'Left-preSMA-S-MATT.nii',
+            'S1_R': 'Right-S1-S-MATT.nii',
+            'S1_L': 'Left-S1-S-MATT.nii',
+            'SMA_R': 'Right-SMA-S-MATT.nii',
+            'SMA_L': 'Left-SMA-S-MATT.nii'}
+
+def custom_brainstem(ifolder, ofolder, thr):
+    cst_r_ifile = os.path.join(ifolder, BRAINSTEM_DCT['CST_R'])
+    cst_l_ifile = os.path.join(ifolder, BRAINSTEM_DCT['CST_L'])
+
+    cst_r_ofile = os.path.join(ofolder, 'brainstem_CST_R.nii.gz')
+    cst_l_ofile = os.path.join(ofolder, 'brainstem_CST_L.nii.gz')
+    cst_ofile = os.path.join(ofolder, 'brainstem_CST.nii.gz')
+
+    cst_r_im, cst_l_im = Image(cst_r_ifile), Image(cst_l_ifile)
+    cst_im = zeros_like(cst_r_im)
+
+    cst_r_im.data[:, :, BRAMSTEM_ZTOP+1:] = 0.
+    cst_l_im.data[:, :, BRAMSTEM_ZTOP+1:] = 0.
+    cst_r_im.data[:, :, :BRAMSTEM_ZBOT] = 0.
+    cst_l_im.data[:, :, :BRAMSTEM_ZBOT] = 0.
+
+    cst_r_im.data[cst_r_im.data > thr] = 1.0
+    cst_r_im.data[cst_r_im.data <= thr] = 0.0
+
+    cst_l_im.data[cst_l_im.data > thr] = 1.0
+    cst_l_im.data[cst_l_im.data <= thr] = 0.0
+
+    cst_im.data = cst_r_im.data + cst_l_im.data
+    cst_im.data[cst_im.data > 0.0] = 1.0
+
+    cst_r_im.save(cst_r_ofile)
+    cst_l_im.save(cst_l_ofile)
+    cst_im.save(cst_ofile)
+    del cst_r_im, cst_l_im, cst_im
+
+
+def custom_brain(ifolder, ofolder):
+    ifname_dct = {}
+    for roi in BRAIN_DCT:
+        ifname_dct[roi] = os.path.join(ifolder, BRAIN_DCT[roi])
+
+    for roi in ifname_dct:
+        ofname = os.path.join(ofolder, 'brain_' + roi + '.nii.gz')
+
+        i_im = Image(ifname_dct[roi])
+        o_im = zeros_like(i_im)
+        o_im.data = i_im.data
+        del i_im
+
+        o_im.data[:, :, :BRAMSTEM_ZTOP+1] = 0.0
+
+        o_im.save(ofname)
+        del o_im
+
+    sum_roi_im = sum([Image(ifname_dct[roi]).data for roi in ifname_dct])
+    i_im = Image(ifname_dct[roi])
+    o_im = zeros_like(i_im)
+    del i_im
+
+    o_im.data[sum_roi_im > 0.0] = 1.0
+    o_im.data[:, :, :BRAMSTEM_ZTOP+1] = 0.0
+
+    o_im.save(os.path.join(ofolder, 'brain.nii.gz'))
+    del o_im
 
 def _check_lesion_seg(fname):
     '''Check if the mask is only made of ones.'''
@@ -106,6 +187,22 @@ def main(args=None):
                                 subj_data_df=subj_data_df)
 
     subj_check_df.to_pickle('0_results.pkl')
+
+    ofolder = config["path_atlases"]
+    if not os.path.isdir(ofolder):
+        os.makedirs(ofolder)
+
+    brainstem_atlas_ifolder = config["path_brainstem_folder"]
+    brainstem_atlas_ofolder = os.path.join(ofolder, 'brainstem')
+    if not os.path.isdir(brainstem_atlas_ofolder):
+        os.makedirs(brainstem_atlas_ofolder)
+        custom_brainstem(brainstem_atlas_ifolder, brainstem_atlas_ofolder, 0.01)
+
+    brain_atlas_ifolder = config["path_smatt_folder"]
+    brain_atlas_ofolder = os.path.join(ofolder, 'brain')
+    if not os.path.isdir(brain_atlas_ofolder):
+        os.makedirs(brain_atlas_ofolder)
+        custom_brain(brain_atlas_ifolder, brain_atlas_ofolder)
 
 
 if __name__ == "__main__":
